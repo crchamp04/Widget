8569bf962802fc23d432397c3441b197ea409091
Chris Champlin <chris.champlin@beyourbest.com>
2021-01-10 17:27:18 +0100

    second commit
    this is a second commit

---
+++ WidgetUnityProject/Assets/Scenes/secondScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //this is the second test script
+
+        //another test
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
